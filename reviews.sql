ee9d10977d96c15a64bc13cca44b0e31bdb8fc45
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-01 14:48:50 +0700

    Add reviews placeholders and new create table query
    new create table query was made because previous query primary ids where coming up null for some reason

--- reviews.sql
+++ reviews.sql
@@ -1,10 +1,18 @@
-INSERT INTO houses (
-    review_id, 
-    user_id, 
-    house_id, 
-    rating, 
-    comment, 
-    date
-)
-VALUES (
-    3, 2, 2, 4.5, 'Wow! Loved my stay here!', 2024-01-24)
+CREATE TABLE reviews (
+    review_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) NOT NULL,
+    house_id INT REFERENCES houses(house_id) NOT NULL,
+    rating FLOAT NOT NULL,
+    comment TEXT NOT NULL,
+    date DATE NOT NULL
+);
+
+
+--Reviews placeholders
+
+INSERT INTO reviews ( user_id, house_id, rating, comment, date )
+VALUES 
+(2, 34, 3.5, 'Good experience', '2024-01-25'),
+(3, 3, 5.0, 'Excellent service', '2024-01-26'),
+(4, 3, 2.0, 'Needs improvement', '2024-01-28'),
+(1, 3, 3.5, 'Average', '2024-01-30')
