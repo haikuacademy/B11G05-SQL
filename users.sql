f75c5007b3afb5fa3b4a37d244709bf4056c1a76
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-01 12:34:47 +0700

    Update users.sql

---
+++ users.sql
@@ -0,0 +1,9 @@
+INSERT INTO 
+users (profile_photo, first_name, last_name, email, password)
+
+VALUES
+('https://randomuser.me/api/portraits/men/65.jpg', 'Vincent',   'Frazier', 'vincent.frazier@example.com', 'africa'), 
+('https://randomuser.me/api/portraits/women/32.jpg', 'Samantha', 'Pearson', 'samantha.pearson@example.com', 'sinclair'), 
+('https://randomuser.me/api/portraits/men/89.jpg', 'Edwin', 'Stewart', 'edwin.stewart@example.com', 'elephant'), 
+('https://randomuser.me/api/portraits/women/31.jpg', 'Monica', 'Gonzalez', 'monica.gonzalez@example.com', 'donna')  
+ 
